8bb2d028ffd620661995a9ebe5fc7cb8c6d12961
Alföldy Bálint <alfoldybalint@lltk.local>
2023-07-11 09:56:43 +0200

    get users feeds

--- handler_user.go
+++ handler_user.go
@@ -41,3 +41,15 @@ func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, 
 
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
+
+func (apiCfg *apiConfig) handlerPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get posts for user: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
+}
